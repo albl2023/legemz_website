eaf11813a8a6b46bd8147164eebd294096b4f35d
God Cat <105698207+albl2023@users.noreply.github.com>
2022-05-17 14:47:01 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function menu_click()
+{
+    var mo = document.getElementById("menu-options");
+    if(mo.style.display === "none") { mo.style.display = "block"; }
+    else { mo.style.display = "none"; }
+}
