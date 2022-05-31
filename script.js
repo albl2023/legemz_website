1e00181081685c9c0ea8ab44adf02f2b77b6479a
God Cat <105698207+albl2023@users.noreply.github.com>
2022-05-31 18:40:58 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function menu_click()
-{
-    var mo = document.getElementById("menu-options");
-    if(mo.style.display === "none") { mo.style.display = "block"; }
-    else { mo.style.display = "none"; }
-}
